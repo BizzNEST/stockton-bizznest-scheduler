197ffc4544e1bdc3e44148da15f4012abfe3a846
Tyler Nguyen <tylernguyen518@gmail.com>
2024-09-24 14:48:33 -0700

    feat(table): table styling/rendering/fetch

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,51 @@
 const modalComponentID = document.getElementById("modalComponentID");
 const startPairingButton = document.getElementById("startPairingButton");
-const closeModalButton = document.getElementById("closeModalButton")
+const closeModalButton = document.getElementById("closeModalButton");
+const tableBody = document.getElementById("tableBody");
 
-startPairingButton.addEventListener("click", () => {
-    modalComponentID.classList.remove("hidden");
-});
 
 closeModalButton.addEventListener("click", () => {
     modalComponentID.classList.add("hidden");
 });
+
+startPairingButton.addEventListener("click", () => {
+    modalComponentID.classList.remove("hidden");
+    loadInterns();
+});
+
+const apiFetch = async () => {
+    const promise = await fetch("./seedata/interns.json");
+    const data = await promise.json();
+    return data;
+}
+
+const loadInterns = async () => {
+    const interns = await apiFetch();
+    
+    const filterInterns = interns.splice(0,10);
+
+    filterInterns.forEach(intern => {
+        const row = document.createElement("tr");
+
+        const checkCol = document.createElement("td");
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkCol.appendChild(checkbox);
+        
+        const nameCell = document.createElement("td");
+        nameCell.textContent = intern.name;
+        
+        const departCol = document.createElement("td");
+        departCol.textContent = intern.department;
+        
+        const locationCol = document.createElement("td");
+        locationCol.textContent = intern.location;
+
+        row.appendChild(checkCol);
+        row.appendChild(nameCell);
+        row.appendChild(departCol);
+        row.appendChild(locationCol);
+
+        tableBody.appendChild(row);
+    });
+}
