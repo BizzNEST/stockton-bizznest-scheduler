c5cf33713d1a7f03959411e5d3cd05af2ba15e2e
Christopher Loya <cloya100@gmail.com>
2024-10-17 17:30:22 -0700

    rename pages

--- scripts/app.js
+++ scripts/app.js
@@ -478,7 +478,7 @@ document.addEventListener("DOMContentLoaded", () => {
             pairInterns();
             isPairingInitiated = false;
             const link = document.createElement('a');
-            link.href = "/Pages/pairings.html";
+            link.href = "/pages/pairings.html";
             document.body.appendChild(link);
             link.click();
         }
